6e658ae70e7e0bae93728a35c3168c50519809b0
Sandhie177 <zszqd@mail.umkc.com>
2018-09-14 16:56:05 -0500

    ICP4
    Code with numpy and employee class

---
+++ ICP4/numpy.py
@@ -0,0 +1,10 @@
+#import numpy modules
+import numpy as np
+#creating a array having random variables from 1 to 20 in size 15
+a=np.random.randint(20,size=(15))
+#Counting the number of time each element present in the array
+counts = np.bincount(a)
+#Print the array
+print (a)
+#Print the element which present maximum times in the array
+print (np.argmax(counts))
