30573805311c1afcd240f2f7cacf2088d303f904
Sandhie177 <zszqd@mail.umkc.com>
2018-09-21 17:45:54 -0500

    ICP5

--- ICP5/ICP5_1_regression.py
+++ ICP5/ICP5_1_regression.py
@@ -1,7 +1,6 @@
 #importing libraries
 import csv
 import numpy as np
-from sklearn import linear_model
 import matplotlib.pyplot as plt
 
 #declaring 2 empty list
@@ -18,23 +17,39 @@ def get_data(filename):
             B.append(float(row[1])) #appending B variables
     return
 
-#function to plot the graph
-def show_plot(A, B):
-    
-    plt.scatter(A, B, color='red')  # plotting the initial datapoints
-    plt.plot(A, linear_mod.predict(A), color='blue', linewidth=3)  # plotting the line made by linear regression
-    plt.show()
-    return
 
 
 get_data('ICP5_1.csv')  # calling get_data method by passing the csv file to it
 
+#getting the maximum and minimum values of x-axix
+min_a= min(*A)-2
+max_a= max(*A)+2
 
-linear_mod = linear_model.LinearRegression()
-A = np.reshape(A, (len(A), 1))  # converting to matrix of n X 1
-B = np.reshape(B, (len(B), 1))
-linear_mod.fit(A, B)  # fitting the data points in the model
+#finding the mean of A and B
+mean_a = np.mean(A)
+mean_b = np.mean(B)
 
-show_plot(A, B) #calling show_plot to show the graph
+# Total number of values
+m = len(A)
+
+# Using the formula to calculate b1 and b2
+numer = 0
+denom = 0
+
+#loop for the equation
+for i in range(m):
+    numer += (A[i] - mean_a) * (B[i] - mean_b)
+    denom += (A[i] - mean_a) ** 2
+b1 = numer / denom
+b0 = mean_b - (b1 * mean_a)
+
+# Print coefficients
+#print(b1, b0)
+
+x = np.linspace(min_a, max_a, 1000)
+y = b0 + b1 * x
+
+plt.scatter(A, B, color='red')
+plt.plot(x, y,color='green') #calling show_plot to show the graph
 
 
