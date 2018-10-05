ee7e15ae5f83373db0321858e3f25d5baf546b09
Sandhie177 <zszqd@mail.umkc.com>
2018-10-05 16:59:55 -0500

    ICP 7

--- ICP7/ICP7_NPL.py
+++ ICP7/ICP7_NPL.py
@@ -1,44 +1,40 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import urllib.request
-import re
 import nltk
 
+#reading a wiki page
 #Get the URl
 url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
 #Put the contents in a variable
 source_code = urllib.request.urlopen(url)
 plain_text = source_code
 soup = BeautifulSoup(plain_text, "html.parser")
-#print (soup)
-para = soup.find_all('p')
-#print (body)
-for link in para:
-    line = link.get_text()
-    print (line)
+file  = open('input.txt', 'w') #opening file to write in
+#print (para)
+for link in soup.find_all('p'):
     
+    file.write(str(link.get_text().encode("utf-8")))
+    
+file.close()
+#reading wiki page ends
 
-def tokens(text):
-    """
-    Get all words from the corpus
-    """
-    return re.findall('[a-z]+', text.lower())
-
-WORDS = tokens(open('big.txt').read())
-##tokenization
-file_content = open("big.txt").read()
-wtokens = nltk.word_tokenize(file_content)
-stokens = nltk.sent_tokenize(file_content)
+##tokenization starts
+file_content = open("input.txt").read()
+wtokens = nltk.word_tokenize(file_content) #word token
+stokens = nltk.sent_tokenize(file_content) #sentence token
 
 print ('\nPerfoming tokenization:\n')
+print ('sentence tokeniztion:\n')
 for s in stokens:
-    print (s)
+    print (s) #printing tokenized sentence
 
-print ('\n') 
+print ('\nWord tokeniztion:') 
 for t in wtokens:
-    print (t)
-
-#stemming
+    print (t) #printing tokenized word
+##tokenization ends
+    
+#stemming starts
 from nltk.stem import PorterStemmer
 from nltk.stem import LancasterStemmer
 from nltk.stem import SnowballStemmer
@@ -46,40 +42,45 @@ from nltk.stem import SnowballStemmer
 #performing stemming
 print ('\nperfoming stemming:')
 for w in wtokens:
-    pStemmer = PorterStemmer()
+    pStemmer = PorterStemmer() #porter stemming
     print (pStemmer.stem(w))
 
-    lStemmer = LancasterStemmer()
+    lStemmer = LancasterStemmer() #lancaster stemming
     print (lStemmer.stem(w))
 
-    sStemmer = SnowballStemmer('english')
+    sStemmer = SnowballStemmer('english') #snowball stemming
     print (sStemmer.stem(w))
-
-#Parts of Speech tagging
+#stemming ends
+    
+#Parts of Speech tagging starts
 print ('\nperfoming POS:')
-text = nltk.word_tokenize(file_content)
-print (nltk.pos_tag(text))
+text = nltk.word_tokenize(file_content) 
+print (nltk.pos_tag(text)) #performing POS tag
+#POS ends
 
-#lemmatization   
+#lemmatization starts  
 print ('\nperforming lemmatization:')    
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
 for w in wtokens:
-    print (lemmatizer.lemmatize(w)) 
-
-#trigram
+    print (lemmatizer.lemmatize(w)) #performing lemmatizer
+#lemmatization ends
+    
+#trigram starts
 print ('\nperforming Trigram:')
 from nltk import ngrams
 
-n = 3
-trigrams = ngrams(file_content.split(), n)
+n = 3 #n defines the number of ngrams
+trigrams = ngrams(file_content.split(), n) #splitting with respect to n
 for grams in trigrams:
   print (grams)
-
-#Named Entity Recognizer
+#trigram ends
+  
+#Named Entity Recognizer starts
 print ('\nPerforming NER:')
 
 from nltk import word_tokenize, pos_tag, ne_chunk
- 
+
 print (ne_chunk(pos_tag(word_tokenize(file_content))))
+#NER ends
